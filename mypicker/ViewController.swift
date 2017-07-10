2f2a9a2b112d04f1f38ccd2021c75856fa57e894
ciecimac <ciecclm@imnu.edu.cn>
2017-07-10 10:58:10 +0800

    滑块
    1.能正常使用的

--- mypicker/ViewController.swift
+++ mypicker/ViewController.swift
@@ -8,16 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
+    @IBOutlet weak var pPicker: UIPickerView!
+    @IBOutlet weak var iPicker: UIPickerView!
+    @IBOutlet weak var dPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pPicker.dataSource = self
+        pPicker.delegate = self
+        iPicker.dataSource = self
+        iPicker.delegate = self
+        dPicker.dataSource = self
+        dPicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    //设置选择框的行数为9行，继承于UIPickerViewDataSource协议
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    
+    //设置选择框各选项的内容，继承于UIPickerViewDelegate协议
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        return String(row)
     }
 
 
